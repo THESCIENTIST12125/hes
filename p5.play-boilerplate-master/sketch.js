48308ae172b0df983f68a8bc5c0a42062c3bea73
THESCIENTIST12125 <64621115+THESCIENTIST12125@users.noreply.github.com>
2020-08-05 13:44:51 +1000

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(800,400);
+ 
+  var h = createSprite(500, 300, 100, 10);
+}
+
+
+b1 = new Ground(600,300,50,50)
+b2 = new Box()
+b3 = new Box()
+b4 = new Box()
+b5 = new Box()
+b6 = new Box()
+b7 = new Box()
+b8 = new Box()
+b9 = new Box()
+b10 = new Box()
+b11= new Box()
+b12= new Box()
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
